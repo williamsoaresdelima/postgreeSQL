1081e453513ecd5411ca6e6301ed2e09e2241b73
williamsoaresdelima <43095193+williamsoaresdelima@users.noreply.github.com>
2021-01-09 22:09:54 -0300

    first commit

---
+++ financas.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS banco(
+	numero INTEGER NOT NULL,
+	nome VARCHAR(50) NOT NULL,
+	ativo BOOLEAN NOT NULL DEFAULT TRUE,
+	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (numero)
+);
+
+CREATE TABLE IF NOT EXISTS agencia(
+	banco_numero INTEGER NOT NULL,
+	numero INTEGER NOT NULL,
+	nome VARCHAR(80) NOT NULL,
+	ativo BOOLEAN NOT NULL DEFAULT TRUE,
+	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (banco_numero, numero),
+	FOREIGN KEY (banco_numero) REFERENCES banco (numero)
+);
+
+CREATE TABLE IF NOT EXISTS cliente(
+	numero BIGSERIAL PRIMARY KEY,
+	nome VARCHAR(120) NOT NULL,
+	email VARCHAR(200) NOT NULL,
+	ativo BOOLEAN NOT NULL DEFAULT TRUE,
+	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS conta_corrente(
+	banco_numero INTEGER NOT NULL,
+	agencia_numero INTEGER NOT NULL,
+	numero BIGINT NOT NULL,
+	digito SMALLINT NOT NULL,
+	cliente_numero BIGINT NOT NULL,
+	ativo BOOLEAN NOT NULL DEFAULT TRUE,
+	PRIMARY KEY (banco_numero,agencia_numero,numero,digito,cliente_numero),
+	FOREIGN KEY (banco_numero, agencia_numero) REFERENCES agencia (banco_numero,numero),
+	FOREIGN KEY (cliente_numero) REFERENCES cliente (numero)	
+);
+
+
+CREATE TABLE IF NOT EXISTS tipo_transacao(
+	id SMALLSERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	ativo BOOLEAN NOT NULL DEFAULT TRUE,
+	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
